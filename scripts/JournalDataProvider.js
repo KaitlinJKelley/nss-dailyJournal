c9bac979ed6fe8c748c1e4bfce82a0897ac1ce15
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-29 10:33:13 -0600

    fetched entry data from local API using fetch; fetch appears in Network

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,29 +1,13 @@
-export const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Meh"
-    },
+let journal = []
 
-    {
-        id: 2,
-        date: "07/26/2025",
-        concept: "Complex Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Help!"
-    },
-
-    {
-        id: 3,
-        date: "07/28/2025",
-        concept: "Complex Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Help!"
-    }
-]
-
+export const getEntries = () => {
+    return fetch("http://localhost:8088/entries") // Fetch from the API
+        .then(response => response.json())  // Parse as JSON
+        .then(entries => {
+            // What should happen when we finally have the array?
+            journal = entries
+        })
+}
 /*
     You export a function that provides a version of the
     raw data in the format that you want
