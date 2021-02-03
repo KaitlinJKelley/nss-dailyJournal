29b9bfe9ad646a019c3258bf59191d3fb0e1522b
kjk1325e <kaitlinjkelley@gmail.comm>
2021-02-02 21:59:35 -0600

    Added Show Journal Entries button and dispatched event listener to show notes when button clicked; need to use NoteStateChanged to add new notes to aside as they're saved

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-import { getEntries } from "./JournalDataProvider.js"
-import { EntryListComponent } from "./JournalentryList.js"
 import { JournalFormComponent } from "./JournalForm.js"
 import "./JournalForm.js"
+import "./ShowEntries/ShowEntriesButton.js"
+import "./ShowEntries/JournalentryList.js"
+import { ShowNotesButton } from "./ShowEntries/ShowEntriesButton.js"
 
-EntryListComponent()
 JournalFormComponent()
+ShowNotesButton()
 
 
